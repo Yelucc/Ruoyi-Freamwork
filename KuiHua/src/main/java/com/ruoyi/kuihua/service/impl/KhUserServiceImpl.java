ad0b1346f3808b688f0b79f04d434c0e0af6d22d
yelucc <413602487@qq.com>
2024-09-16 14:11:21 +0800

    perf(葵花): 用户注册和登录

--- KuiHua/src/main/java/com/ruoyi/kuihua/service/impl/KhUserServiceImpl.java
+++ KuiHua/src/main/java/com/ruoyi/kuihua/service/impl/KhUserServiceImpl.java
@@ -39,6 +39,7 @@ import com.ruoyi.kuihua.service.KhUserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
 import javax.annotation.Resource;
+import java.util.Objects;
 
 /**
  * 用户管理Service业务层处理
@@ -76,7 +77,47 @@ public class KhUserServiceImpl extends ServiceImpl<KhUserMapper, KhUser>
                 .eq(KhUser::getPhone, body.getUsername())
                 .or()
                 .eq(KhUser::getWechatNumber, body.getUsername()));
-        return loginService.login(khUser.getPhone(),body.getPassword(),body.getCode(),body.getUuid());
+        if (Objects.isNull(khUser)){
+            throw new RuntimeException("葵花用户不存在");
+        }
+        String username = body.getUsername();
+        String password = body.getPassword();
+
+//        // 验证码校验
+//        validateCaptcha(username, body.getCode(), body.getUuid());
+        // 登录前置校验
+        loginService.loginPreCheck(username, password);
+        // 用户验证
+        Authentication authentication = null;
+        try
+        {
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+            AuthenticationContextHolder.setContext(authenticationToken);
+            // 该方法会去调用UserDetailsServiceImpl.loadUserByUsername
+            authentication = authenticationManager.authenticate(authenticationToken);
+        }
+        catch (Exception e)
+        {
+            if (e instanceof BadCredentialsException)
+            {
+                AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, MessageUtils.message("user.password.not.match")));
+                throw new UserPasswordNotMatchException();
+            }
+            else
+            {
+                AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, e.getMessage()));
+                throw new ServiceException(e.getMessage());
+            }
+        }
+        finally
+        {
+            AuthenticationContextHolder.clearContext();
+        }
+        AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_SUCCESS, MessageUtils.message("user.login.success")));
+        LoginUser loginUser = (LoginUser) authentication.getPrincipal();
+        recordLoginInfo(loginUser.getUserId());
+        // 生成token
+        return tokenService.createToken(loginUser);
     }
 
     /**
